9bed8a6fb4b5caa303a5960a3299bd7166818529
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-10-16 20:28:06 -0300

    ResponseEntity (OK)

--- child-play/child-play/src/main/java/br/com/ecommerce/childplay/model/ResponseEntity.java
+++ child-play/child-play/src/main/java/br/com/ecommerce/childplay/model/ResponseEntity.java
@@ -2,7 +2,6 @@ package br.com.ecommerce.childplay.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.io.Serializable;
-import java.util.Date;
 
 @JsonInclude(content = JsonInclude.Include.NON_NULL, value = JsonInclude.Include.NON_DEFAULT)
 public class ResponseEntity implements Serializable {
@@ -10,34 +9,8 @@ public class ResponseEntity implements Serializable {
     public static class Response {
 
         private static final long serialVersionUID = -5393192543608035163L;
-        private long id;
-        private long epochTime;
-        private int returnId;
+
         private String returnMsg;
-
-        public long getId() {
-            return id;
-        }
-
-        public void setId(long id) {
-            this.id = id;
-        }
-
-        public long getEpochTime() {
-            return epochTime;
-        }
-
-        public void setEpochTime(long epochTime) {
-            this.epochTime = epochTime;
-        }
-
-        public int getReturnId() {
-            return returnId;
-        }
-
-        public void setReturnId(int returnId) {
-            this.returnId = returnId;
-        }
 
         public String getReturnMsg() {
             return returnMsg;
@@ -50,22 +23,20 @@ public class ResponseEntity implements Serializable {
         public Response() {
         }
 
-        public Response(long epochTime, int returnId, String returnMsg) {
-            this.epochTime = epochTime;
-            this.returnId = returnId;
+        public Response(String returnMsg) {
             this.returnMsg = returnMsg;
         }
 
         public static Response createSuccess() {
-            return new Response(new Date().getTime(), 0, "Success.");
+            return new Response( "Success.");
         }
 
         public static Response createUnknownError() {
-            return new Response(new Date().getTime(), 99, "Unknown error.");
+            return new Response( "Unknown error.");
         }
 
         public static Response createError(Exception e) {
-            return new Response(new Date().getTime(), 99, e.getMessage());
+            return new Response( e.getMessage());
         }
     }
 
@@ -79,8 +50,8 @@ public class ResponseEntity implements Serializable {
         this.response = response;
     }
 
-    public ResponseEntity(long epochTime, int returnId, String returnMsg) {
-        this.response = new Response(epochTime, returnId, returnMsg);
+    public ResponseEntity( String returnMsg) {
+        this.response = new Response(returnMsg);
     }
 
     public static ResponseEntity createSuccess() {
