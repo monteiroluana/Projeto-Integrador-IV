e7a8c38b427e4eb5f353b4e240be863927e9f952
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-10-29 21:49:49 -0300

    teste gerando boleto

---
+++ child-play/child-play/src/main/java/br/com/ecommerce/childplay/z/zDay.java
@@ -0,0 +1,89 @@
+package br.com.ecommerce.childplay.z;
+
+import br.com.caelum.stella.boleto.Banco;
+import br.com.caelum.stella.boleto.Beneficiario;
+import br.com.caelum.stella.boleto.Boleto;
+import br.com.caelum.stella.boleto.Datas;
+import br.com.caelum.stella.boleto.Endereco;
+import br.com.caelum.stella.boleto.Pagador;
+import br.com.caelum.stella.boleto.bancos.BancoDoBrasil;
+import br.com.caelum.stella.boleto.transformer.GeradorDeBoleto;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zday")
+public class zDay {
+
+    @RequestMapping("/z")
+    public void generateReport(HttpServletResponse response) throws IOException {
+
+        Datas datas = Datas.novasDatas()
+                .comDocumento(1, 5, 2008)
+                .comProcessamento(1, 5, 2008)
+                .comVencimento(2, 5, 2008);
+
+        Endereco enderecoBeneficiario = Endereco.novoEndereco()
+                .comLogradouro("Av das Empresas, 555")
+                .comBairro("Bairro Grande")
+                .comCep("01234-555")
+                .comCidade("São Paulo")
+                .comUf("SP");
+
+        //Quem emite o boleto
+        Beneficiario beneficiario = Beneficiario.novoBeneficiario()
+                .comNomeBeneficiario("D-Evolution")
+                .comAgencia("xxxx").comDigitoAgencia("0")
+                .comCodigoBeneficiario("76000")
+                .comDigitoCodigoBeneficiario("5")
+                .comNumeroConvenio("1207113")
+                .comCarteira("18")
+                .comEndereco(enderecoBeneficiario)
+                .comNossoNumero("1155661133");
+
+        //Quem paga o boleto
+        Endereco enderecoPagador = Endereco.novoEndereco()
+                .comLogradouro("Av dos testes, 111 apto 333")
+                .comBairro("Bairro Teste")
+                .comCep("01234-111")
+                .comCidade("São Paulo")
+                .comUf("SP");
+
+        Pagador pagador = Pagador.novoPagador()
+                .comNome("Fulano da Silva")
+                .comDocumento("111.222.333-12")
+                .comEndereco(enderecoPagador);
+
+        Banco banco = new BancoDoBrasil();
+
+        Boleto boleto = Boleto.novoBoleto()
+                .comBanco(banco)
+                .comDatas(datas)
+                .comBeneficiario(beneficiario)
+                .comPagador(pagador)
+                .comValorBoleto("200.00")
+                .comNumeroDoDocumento("1234")
+                .comInstrucoes("instrucao 1", "instrucao 2", "instrucao 3", "instrucao 4", "instrucao 5")
+                .comLocaisDePagamento("local 1", "local 2");
+
+        System.out.println("número código de barras: " + boleto.getCodigoDeBarras());
+
+        GeradorDeBoleto gerador = new GeradorDeBoleto(boleto);
+        byte[] bPDF = gerador.geraPDF();
+
+        streamReport(response, bPDF, "my_report.pdf");
+    }
+
+    protected void streamReport(HttpServletResponse response, byte[] data, String name)
+            throws IOException {
+
+        response.setContentType("application/pdf");
+        response.setHeader("Content-disposition", "attachment; filename=" + name);
+        response.setContentLength(data.length);
+
+        response.getOutputStream().write(data);
+        response.getOutputStream().flush();
+    }
+}
