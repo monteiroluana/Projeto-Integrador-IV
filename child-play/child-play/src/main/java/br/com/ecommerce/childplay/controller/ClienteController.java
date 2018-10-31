dcbb1697b1f6a2f686a590146962fd9425844ea4
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-10-31 18:20:32 -0300

    cliente save (OK)

--- child-play/child-play/src/main/java/br/com/ecommerce/childplay/controller/ClienteController.java
+++ child-play/child-play/src/main/java/br/com/ecommerce/childplay/controller/ClienteController.java
@@ -1,4 +1,3 @@
-
 package br.com.ecommerce.childplay.controller;
 
 import br.com.ecommerce.childPlay.model.Cliente;
@@ -6,29 +5,35 @@ import br.com.ecommerce.childplay.model.ResponseEntity;
 import br.com.ecommerce.childplay.service.ClienteService;
 import java.sql.SQLException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
-    
-    @PostMapping("/save")
-    public ResponseEntity saveProduto(@ModelAttribute("cliente")Cliente cliente) throws ClassNotFoundException, SQLException {
-       ClienteService service = new ClienteService();
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ResponseEntity saveProduto(@RequestBody Cliente cliente) throws ClassNotFoundException, SQLException {
+        ClienteService service = new ClienteService();
         ResponseEntity re = null;
+        System.out.println("\n --------");
+         System.out.println("resposta URL:cliente/save: " + cliente.getNome());
+         System.out.println("resposta URL:cliente/save: " + cliente.getLogin());
+         System.out.println("resposta URL:cliente/save: " + cliente.getSenha());
+         System.out.println("resposta URL:cliente/save: " + cliente.getCpf());
         try {
             re = ResponseEntity.createSuccess();
             re.setData(service.saveCliente(cliente));
         } catch (ClassNotFoundException | SQLException e) {
             re = ResponseEntity.createUnknownError();
         }
-      return re;
+        return re;
+       
     }
-    
+
     @GetMapping("/form")
     public ModelAndView abrirForm() {
         ModelAndView retorno = new ModelAndView("telaTesteCliente")
