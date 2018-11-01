ded7180e652100ca715b40c9d7fb48d6eda04ff5
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-11-01 10:44:13 -0300

    list clientes /get cliente by login&senha (dao)

--- child-play/child-play/src/main/java/br/com/ecommerce/childplay/dao/ClienteDAO.java
+++ child-play/child-play/src/main/java/br/com/ecommerce/childplay/dao/ClienteDAO.java
@@ -5,9 +5,117 @@ import br.com.ecommerce.childPlay.model.Cliente;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Date;
 
 public class ClienteDAO {
 
+    public List<Cliente> listClientes() throws ClassNotFoundException, SQLException {
+        String sql = "SELECT * FROM Cliente WHERE enable = ?";
+
+        List<Cliente> lista = new ArrayList<>();
+
+        Connection connection = null;
+        ResultSet rs = null;
+        PreparedStatement p = null;
+
+        try {
+            connection = Conexao.getConnection();
+            p = connection.prepareStatement(sql);
+            p.setBoolean(1, true);
+            //Armazenando os resultados
+            rs = p.executeQuery();
+
+            //Enquanto tiver linha de resultado execute esse trecho
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                Cliente.setIdCliente(rs.getInt("idCliente"));
+                Cliente.setNome(rs.getString("nome"));
+                cliente.setCpf(rs.getString("cpf"));
+                cliente.setDataNasc(rs.getDate("dataNasc"));
+                cliente.setStringGenero(rs.getString("genero"));
+                cliente.setString(rs.getString("telefone"));
+                cliente.setEmail(rs.getString("email"));
+                Cliente.setLogin(rs.getString("login"));
+                Cliente.setSenha(rs.getString("senha"));
+ 
+                lista.add(Cliente);
+            }
+        } catch (SQLException e) {
+
+        } finally {
+            //Fechando todas as conexões que foram abertas
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (p != null) {
+                    p.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+
+            }
+        }
+        return lista;
+    }
+    
+    public Cliente getClienteByLoginSenha(String login, String senha) throws ClassNotFoundException, SQLException {
+        String sql = "SELECT * FROM Cliente WHERE enable = ? and login = ? and senha = ?";
+
+        List<Cliente> lista = new ArrayList<>();
+
+        Connection connection = null;
+        ResultSet rs = null;
+        PreparedStatement p = null;
+
+        try {
+            connection = Conexao.getConnection();
+            p = connection.prepareStatement(sql);
+            p.setBoolean(1, true);
+            p.setString(2, cliente.getLogin());
+            p.setString(3, cliente.getSenha());
+            //Armazenando os resultados
+            rs = p.executeQuery();
+
+            //Enquanto tiver linha de resultado execute esse trecho
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                Cliente.setIdCliente(rs.getInt("idCliente"));
+                Cliente.setNome(rs.getString("nome"));
+                cliente.setCpf(rs.getString("cpf"));
+                cliente.setDataNasc(rs.getDate("dataNasc"));
+                cliente.setStringGenero(rs.getString("genero"));
+                cliente.setString(rs.getString("telefone"));
+                cliente.setEmail(rs.getString("email"));
+                Cliente.setLogin(rs.getString("login"));
+                Cliente.setSenha(rs.getString("senha"));
+ 
+                lista.add(Cliente);
+            }
+        } catch (SQLException e) {
+
+        } finally {
+            //Fechando todas as conexões que foram abertas
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (p != null) {
+                    p.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+
+            }
+        }
+        return lista;
+    }
+    
+    
     public boolean saveCliente(Cliente cliente) throws SQLException {
 
         String sql = "INSERT INTO cliente (nome, cpf, dataNasc, genero, telefone, email, login, senha, enable) VALUES (?,?,?,?,?,?,?,?,?);";
