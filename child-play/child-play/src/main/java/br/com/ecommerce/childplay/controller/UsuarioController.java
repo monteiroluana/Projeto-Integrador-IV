e8609d70f74e7ea3621d32269ffd003f49ac060f
Bruna Sayuri <bruna_susuke@hotmail.com>
2018-12-03 20:47:28 -0200

    restcontroller :balloon:

--- child-play/child-play/src/main/java/br/com/ecommerce/childplay/controller/UsuarioController.java
+++ child-play/child-play/src/main/java/br/com/ecommerce/childplay/controller/UsuarioController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+//@RestController
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
