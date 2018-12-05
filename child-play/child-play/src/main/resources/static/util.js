e2c41d185d88990908d4bdc6495bdf78c335e393
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-12-05 19:16:55 -0200

    modelo de tela

---
+++ child-play/child-play/src/main/resources/static/util.js
@@ -0,0 +1,9 @@
+$('.sidenav').append(
+
+   '<a href = "#" > Usuários</a>'+
+    '<a href="#">Produtos</a>'+
+    '<a href="#">Pedidos</a>'+
+    '<a href="#">Relatório</a>'+
+   ' <a href="#">Descontos</a>'
+
+);
