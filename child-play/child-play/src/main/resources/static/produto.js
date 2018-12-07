51f99e0c283cb1aecddb2324eee2ee6da30d581b
Luana Monteiro <luanamonteiropereira95@gmail.com>
2018-12-07 21:58:19 -0200

    ...

--- child-play/child-play/src/main/resources/static/produto.js
+++ child-play/child-play/src/main/resources/static/produto.js
@@ -1,6 +1,7 @@
 let listProdutos = []
 
 $(document).ready(function () {
+
     $.ajax({
         url: '/produto/list-produto', method: 'GET', success: function (data) {
             listProdutos = data.data;
@@ -27,11 +28,11 @@ $(document).ready(function () {
                     '            <span>' + listProdutos[i].desconto + '</span>' +
                     '        </td >' +
 
-                    '<td>' +
-                    '    <button type="button" class="btn btn-outline-success" onclick="abrirModal(' + listProdutos[i].idProduto + ')">Detalhes</button>' +
-                    '    <button type="button" class="btn btn-outline-info">Editar</button>' +
-                    '    <button type="button" class="btn btn-outline-danger">Excluir</button>' +
-                    '</td>' +
+                    '        <td>' +
+                    '           <button type="button" class="btn btn-outline-success" onclick="abrirModal(' + listProdutos[i].idProduto + ')" title="Ver mais detalhes do produto ' + listProdutos[i].nome + '">Detalhes</button>' +
+                    '           <button type="button" class="btn btn-outline-info" title="Editar o produto ' + listProdutos[i].nome + '">Editar</button>' +
+                    '           <button type="button" class="btn btn-outline-danger" title="Excluir o produto ' + listProdutos[i].nome + '">Excluir</button>' +
+                    '        </td>' +
 
                     '    </tr >'
 
@@ -41,25 +42,43 @@ $(document).ready(function () {
     });
 });
 
+
+//------Modal - ver mais detalhes do produto ----------------------------
+
 function abrirModal(idProduto) {
     let i = idProduto - 1;
     let imgs = listProdutos[i].imagem.length;
 
     // imagens
-    document.querySelector(".carousel-inner").innerHTML='';
+    document.querySelector(".carousel-inner").innerHTML = '';
     document.querySelector(".modal-title").innerHTML = listProdutos[i].nome;
     $('.carousel-inner').append(
-        '<div class="carousel-item active">'+
-        '    <img class="d-block w-100" src="' + listProdutos[i].imagem[0].imagem + '" alt="First slide" width=100px>'+
+        '<div class="carousel-item active">' +
+        '    <img class="d-block w-100" src="' + listProdutos[i].imagem[0].imagem + '" alt="First slide">' +
         '</div>'
     );
-    for(var j=1; j < imgs; j++){
+    for (var j = 1; j < imgs; j++) {
         $('.carousel-inner').append(
-            '<div class="carousel-item">'+
-            '    <img class="d-block w-100" src="' + listProdutos[i].imagem[j].imagem + '" alt="First slide" width=100px>'+
+            '<div class="carousel-item">' +
+            '    <img class="d-block w-100" src="' + listProdutos[i].imagem[j].imagem + '" alt="First slide">' +
             '</div>'
         );
     }
 
+    document.querySelector(".nome").value = listProdutos[i].nome;
+    document.querySelector(".nome").disabled = true;
+
+    document.querySelector(".marca").value = listProdutos[i].marca;
+    document.querySelector(".marca").disabled = true;
     $('#detalhes').modal();
 }
+
+$('#tab a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
+
+$('#tab a[href="#profile"]').tab('show') // Selecione a tab pelo nome
+$('#tab a:first').tab('show') // Selecione a primeira tab
+$('#tab a:last').tab('show') // Selecione a última tab
+$('#tab li:eq(2) a').tab('show') // Selecione a terceira tab (0-indexed)
